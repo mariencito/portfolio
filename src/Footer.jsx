6f8388013687685d4fe45884a81e2c5136481c12
marien <mariencastellanosjr@gmail.com>
2023-11-12 20:28:57 -0500

    changed footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,10 +6,9 @@ function Footer() {
 
   return (
     <>
-    <div id="container">
+    <div id="footer">
       <a href="https://github.com/mariencito"><img id="github" src="github.png"></img></a>
       <a href="https://www.linkedin.com/in/marien-castellanos-274346191/"><img id="linkin" src="linkin.png"></img></a>
-      <a href="https://twitter.com/mariencito"><img id="twitter" src="tweet.png"></img></a>
       </div>
     </>
     
