fd3c6821e29b89b43faf37f4793268509a348769
marien <mariencastellanosjr@gmail.com>
2023-11-13 16:59:18 -0500

    about to start backend

--- src/About.jsx
+++ src/About.jsx
@@ -30,7 +30,7 @@ function About() {
       <div className="about-text">
         <h2>"A UI is like a good joke. If you have to explain it, it's not that good."</h2>
         <p>
-        My name is Marien Castellanos, and I am a Full-Stack Developer. I used to be an electronics repair technician. I decided to join the coding bootcamp at the University of Miami to learn how to code quicker. I earned my associate degree, and I am working toward my bachelor's degree in Computer Science. I am ready to improve your business.
+        My name is Marien Castellanos, I am a Full-Stack Developer. I used to be an electronics repair technician. I decided to join the coding bootcamp at the University of Miami to learn how to code quicker. I earned my associate degree, and I am working toward my bachelor's degree in Computer Science. I am ready to improve your business.
         </p>
       </div>
     </div>
